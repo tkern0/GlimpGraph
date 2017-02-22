99f120f0f1e91fd909d99731fa6a83ebeb0e66e7
tkern0 <tkern9182@gmail.com>
2017-02-22 21:57:39 +1300

    Inital commit

---
+++ GlimpGraph.py
@@ -0,0 +1,124 @@
+from datetime import date
+from datetime import timedelta as td
+import matplotlib.backends.backend_tkagg
+import matplotlib.pyplot as plt
+import os.path
+import re
+from sys import argv
+
+# Gets all data from a selected day
+# Day must be in the format DD/MM/YYYY
+def getDay(dir, day):
+    sensor, meter = [None for i in range(96)], []
+    with open(dir) as f:
+        for line in f:
+            if line.split(";")[1].startswith(day):
+                sLine = line.split(";")
+                if sLine[6] == "0":
+                    meter.append({"T": getDayPart(sLine[1].split(" ")[1]),
+                                  "B": float(sLine[5])})
+                else:
+                    sensor[getDayPart(sLine[1].split(" ")[1][:5])] = float(sLine[5])
+    return sensor, meter
+
+# Takes time inputed in the format HH.MM
+# Returns which 15m section of the day it is part of
+# This is used to make averages possible
+def getDayPart(t):
+    s = str(t).split(".")
+    return int(int(s[1])/15) + int(s[0]) * 4
+
+# Averages the values of several days
+def getAverageDay(days):
+    tBGL = [[] for i in range(96)]
+    for day in days:
+        for dPart in range(96):
+            tBGL[dPart].append(day[dPart])
+    fBGL = [None for i in range(96)]
+    for i in range(96): fBGL[i] = meanNone(tBGL[i])
+    return fBGL
+
+# The file comes with nul characters which causes problems
+# This removes them
+def fixFile(dir, dir2=None):
+    if not dir2: dir2=dir
+    fi = open(dir, 'rb')
+    data = fi.read()
+    fi.close()
+    fo = open(dir2, 'wb')
+    fo.write(data.replace(b'\x00', b''))
+    fo.close()
+
+# Coverts from mg/dL to mmoL/L
+def convertMMOLL(levels):
+    nLevels = []
+    for i in levels:
+        if i == None: nLevels.append(None)
+        else: nLevels.append(i/18)
+    return nLevels
+
+# Rounds all the values in a list to 1 decimal
+def roundLevels(levels):
+    nLevels = []
+    for i in levels:
+        if i == None: nLevels.append(None)
+        else: nLevels.append(round(i, 1))
+    return nLevels
+
+# Gets the mean of a list, ignoring 'None's
+def meanNone(l): 
+    a = [i for i in l if not i == None]
+    if not a == []: return sum(a)/len(a)
+    else: return None
+# Getting some starting vars
+if len(argv) < 2:
+    print("Input the location of 'GlicemiaMisurazioni.csv'")
+    f = input("> ")
+else: f = argv[1]
+while not os.path.isfile(f) and os.path.basename(f) == "GlicemiaMisurazioni.csv":
+    print("Incorrect file or file does not exist")
+    f = input("GlicemiaMisurazioni.csv location: ")
+fixFile(f)
+print("Do you want to convert levels to mmol/L? [Y/N] ")
+mi = input("> ").lower()
+while mi not in ("y", "n", "yes", "no"):
+    print("Invalid input. Please enter 'Y' or 'N'")
+    print("Do you want to convert levels to mmol/L? [Y/N] ")
+    mi = input("> ").lower()
+mmolL = mi in ("y", "yes")
+allowedIn = re.compile("^([0-9]{1,2}\\/[0-9]{1,2}\\/([0-9]{2}|[0-9]{4})|last[0-9]+|q|quit)$", re.IGNORECASE)
+currentDay = date.today()
+print("Input a date, in the format DD/MM/YYYY, to get BGLs from that date")
+print("Input 'last[x]', where '[x]' is any positive interger, to get an average of the last '[x]' days")
+print("Input 'Q' or 'Quit' to quit")
+# Main loop
+while True:
+    day = input("> ").lower()
+    if allowedIn.match(day):
+        if day in ("q", "quit"): break
+        elif day.startswith("last"):
+            checkDays, levels = [], []
+            firstDay = currentDay - td(days=int(day[4:]))
+            for i in range((currentDay-firstDay).days + 1): checkDays.append((firstDay + td(days=i)).strftime("%d/%m/%Y"))
+            for i in checkDays:
+                s, m = getDay(f, i)
+                levels.append(s)
+            avLevels = roundLevels(convertMMOLL(getAverageDay(levels)))
+            plt.plot(avLevels, "r-")
+            plt.title("Average of {} to {}".format(firstDay, currentDay))
+            plt.xlabel("Part of day (1 = 15m)")
+            if mmolL: plt.ylabel("BGL (mmol/L)")
+            else: plt.ylabel("BGL (mg/dL)")
+            plt.show()
+        else:
+            sDay = day.split("/")
+            fDay = sDay[0].zfill(2) + "/" + sDay[1].zfill(2) + "/" + str("20" if len(sDay[2]) == 2 else "") + sDay[2]
+            s, m = getDay(f, fDay)
+            s = roundLevels(convertMMOLL(s))
+            plt.plot(s, "r-")
+            plt.plot([i["T"] for i in m], roundLevels(convertMMOLL([i["B"] for i in m])), "ro")
+            plt.title(fDay)
+            if mmolL: plt.ylabel("BGL (mmol/L)")
+            else: plt.ylabel("BGL (mg/dL)")
+            plt.show()
+    else: print("Invalid input")
---
+++ README.md
@@ -0,0 +1,43 @@
+# GlimpGraph
+A PC interpreter of BGLs from the app Glimp.
+
+## Setup
+This assumes you already have Glimp setup to read from your sensor.
+
+#### Method A
+1. Connect your phone to your PC as a media device  
+
+2. Browse to the phone, and from the base directory copy the file 'GlicemiaMisurazioni.csv' elsewhere on your PC
+
+3. Download the latest release of this program from [here](https://github.com/tkern0/GlimpGraph/releases).  
+   This is a single .exe which you can store anywhere.
+
+4. Drag and drop 'GlicemiaMisurazioni.csv' onto this exe.  
+   ![PC Usage](https://github.com/tkern0/GlimpGraph/blob/master/Images/05%20PC%20Usage.PNG)  
+   The program will start and guide you through the rest.
+
+#### Method B
+1. Connect Glimp to Dropbox  
+   Tap the drop down menu and go to 'Options' then 'Remote glucose monitoring'.
+   ![Remote glucose monitoring](https://github.com/tkern0/GlimpGraph/blob/master/Images/01%20Remote%20glucose%20monitoring.png)  
+   Tap Dropbox. This will bring up a window in your browser.  
+   ![Dropbox login](https://github.com/tkern0/GlimpGraph/blob/master/Images/02%20Dropbox%20login.png)  
+   Either create a Dropbox account, log into one, or log into a Google Account.  
+   Allow Glimp access.  
+   ![Permission](https://github.com/tkern0/GlimpGraph/blob/master/Images/03%20Permission.png)  
+   You will be returned to Glimp. If everything's gone correctly you should see "Connected" under the Dropbox heading. 
+   ![Connected](https://github.com/tkern0/GlimpGraph/blob/master/Images/04%20Connected.png)  
+
+2. Get access to the file on your PC.  
+   The specific file you want is 'GlicemiaMisurazioni.csv'.  
+   This program slightly modifies the file, so it is probably best to copy it elsewhere before running it.  
+   You have two options here.
+   1. Log into Dropbox [here](https://www.dropbox.com), and every time you want to use this download the file.
+   2. Download and install the Dropbox desktop app from [here](https://www.dropbox.com/download). This will automatically sync the files whenever they are updated.
+
+3. Download the latest release of this program from [here](https://github.com/tkern0/GlimpGraph/releases).  
+   This is a single .exe which you can store anywhere.
+
+4. Drag and drop 'GlicemiaMisurazioni.csv' onto this exe.  
+   ![PC Usage](https://github.com/tkern0/GlimpGraph/blob/master/Images/05%20PC%20Usage.PNG)  
+   The program will start and guide you through the rest.
